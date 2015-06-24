21bbb1e5c0429776146841e71ff3d5a5cd29f2b2
Mark Pinese <mark.pinese@gmail.com>
2015-06-24 15:21:20 +1000

    bamboo: simplified build harness, modifying for app-only use with defined resources package.

--- build.sh
+++ build.sh
@@ -1,41 +1,17 @@
 #!/bin/bash
-# test harness to buid an applet, and run a small test
-# usage $0 <run|deploy|rm>
-
-# exit on any error
 set -x -e -o pipefail
 
 function dxlogin {
-#  which dx
   source $HOME/dx-toolkit/environment
   dx login --noprojects --token=${bamboo_DX_TOKEN}
 }
 
-function create_project {
-  dx new project -s ${PROJ_NAME}
+# Deploy, but do not publish an app, for testing
+function build_smoketest_app {
+  dx build . --version smoketest --app --yes
 }
 
-# build an applet.
-function build_applet {
-  dx build .
-  dx mkdir /assets
-  dx cp kccg:/assets/kccg_validation_reporter_resources_bundle-1.0.tar.gz /assets/
-  dx cp kccg:/assets/R-3.2.0.compiled.packages_v2.tar.gz /assets/
-  dx cp kccg:/assets/BSgenome.HSapiens.1000g.37d5_1.0.0.tar.gz /assets/
-}
-
-# deploy and publish an applet. - ie install it to kccg
-function publish_applet {
-  dx select kccg
-  dx build -f
-}
-
-# deploy, but do not publish an app. This can be edited prior to publish()ing
-function build_app {
-  dx build . --version 9.9.9 --app --yes
-}
-
-# deploy and publish an app. This is irreversible (but the release can be deprecated).
+# Deploy and publish an app. This is irreversible (but the release can be deprecated).
 function publish_app {
   if [[ -n "${VERSION}" ]]; then
     dx build . --version ${VERSION} --app --publish --yes
@@ -45,11 +21,11 @@ function publish_app {
   fi
 }
 
-
-function smoketest {
+# Run a basic test of a deployed app.
+function run_smoketest {
   vcfgz=project-Bb9KVk8029vp1qzXz4yx4xB3:file-BbBg1g00z1f8vYJq606Yjv7K
   region=project-BZ4JvjQ0K74XK3bP71gykXKQ:file-Bf4JFV00K74xkqBP2Qzbq1p4
-  jobid=$(dx run /kccg-validation-reporter-dx -ivcfgz=$vcfgz -iregion=$region -iextended=true --yes --brief)
+  jobid=$(dx run app-kccg-validation-reporter-dx/smoketest -ivcfgz=$vcfgz -iregion=$region -iextended=true --yes --brief)
 
   #
   # consistent to all applets
@@ -63,46 +39,11 @@ function smoketest {
 }
 
 
-function rmproject {
-   dx rmproject -y $PROJ_NAME
-}
-
-if [[ $1 = "run" ]]; then 
+if [[ $1 = "smoketest" ]]; then 
   dxlogin
-  create_project
-  build_applet
-  smoketest
+  build_smoketest_app
+  run_smoketest
 elif [[ $1 = "publish" ]]; then
   dxlogin
-  publish_applet
-elif [[ $1 = "rm" ]]; then
-  dxlogin
-  rmproject
+  publish_app
 fi
-
-########################################
-# Creating BAMBOO Build & Deploy Plans #
-########################################
-# SOP for Creating a Build Plan
-# Create : Clone an existing Deploy plan: choose kccg-bwa-mem's deploy plan
-# Edit stash repository
-# Double check the stages
-# run.
-
-# SOP for Creating a Deployment Project
-# Creating a Build Plan
-# Environment: DNANexus
-#
-# 1) Clean Working Dir
-#
-# 2) Artifact Download
-#    Artifact Name: app
-#
-# 3) Script
-#    Task description: make executable
-#    Script body: chmod +x resources/usr/bin/*
-#
-# 4) Command
-#    Task description: publish app
-#    Argument: bash build.sh publish
-#    Environment variables: VERSION=${bamboo.deploy.version}
