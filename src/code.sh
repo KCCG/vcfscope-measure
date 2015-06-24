12041b3eb3a8e4127045fd8502718a7656e648fc
Mark Pinese <mark.pinese@gmail.com>
2015-06-24 12:25:26 +1000

    dx: Initial changes to transform to app.

--- src/code.sh
+++ src/code.sh
@@ -12,17 +12,32 @@ main() {
   dx-download-all-inputs --parallel
 
   #
+  # Locate the assets bundle, the location of which varies, depending on whether
+  # this is an app or an applet.
+  #
+  if [[ "$DX_RESOURCES_ID" != "" ]]; then
+    # This is an app; fetch assets from the app's private asset container
+    DX_ASSETS_ID="$DX_RESOURCES_ID"
+  else
+    # This is an applet; fetch assets from the parent project
+    DX_ASSETS_ID="$DX_PROJECT_CONTEXT_ID"
+  fi
+
+  #
   # Stream and unpack assets bundle
   #
-  [[ -n "$DX_RESOURCES_ID" ]] && DX_ASSETS_ID="$DX_RESOURCES_ID" || DX_ASSETS_ID="$DX_PROJECT_CONTEXT_ID"
   mkdir ~/resources
   cd ~/resources
   dx cat "${DX_ASSETS_ID}:/assets/kccg_validation_reporter_resources_bundle-1.0.tar.gz" | tar zxf - # => functional_regions/* gold_standard/* kccg/* mask_regions/* orig/*
-  cd ~
 
   #
   # setup R
   #
+  cd ~
+  # This R is Aaron's R-3.2.0 with pre-installed packages, with the following 
+  # additional packages pre-installed:
+  # CRAN: inline, RSQLite, png, gsalib
+  # BioC: VariantAnnotation, GenomicRanges, BSgenome
   dx cat "${DX_ASSETS_ID}:/assets/R-3.2.0.compiled.packages_v2.tar.gz" | tar -zxf -
   export PATH="$PWD/bin:$PATH"
   export RHOME=${HOME} # This is needed to make RScript work, since it was compiled in a different dir.
@@ -33,7 +48,7 @@ main() {
   #
   # process options
   #
-  if [ "extended" == "true" ]; then
+  if [ "${extended}" == "true" ]; then
     opts+=("-x")
   fi
   if [ -n "${region}" ]; then
@@ -51,28 +66,4 @@ main() {
   #
   dx-upload-all-outputs
   propagate-user-meta vcfgz report
-}
-
-#
-# do this only once, during development.
-# It facilitates both the Validation Report, and R plots
-#
-dev_update_aaron_R () {
-  mkdir R-tmp; cd R-tmp
-  dx cat "$DX_PROJECT_CONTEXT_ID:/assets/R-3.2.0.compiled.packages_v1.tar.gz" | tar -zxf -
-  sed -i "s%/home/dnanexus%$PWD%" bin/R # Set R_HOME_DIR
-  bin/R --quiet -e 'install.packages(c("inline", "RSQLite", "png", "gsalib"), repos="http://cran.rstudio.com")'
-  # sudo apt-get install libxml2-dev
-  bin/R --quiet -e 'source("http://bioconductor.org/biocLite.R"); biocLite(c("VariantAnnotation", "GenomicRanges", "BSgenome"))'
-  bin/R --quiet -e 'paste(sort(rownames(installed.packages())), collapse=", ")'
-  tar -pzcf ../R-3.2.0.compiled.packages_v2.tar.gz ./
-  cd ..
-  dx upload --path /assets/ R-3.2.0.compiled.packages_v2.tar.gz
-  rm -rf R-tmp
-}
-
-build_R311 () {
-sudo apt-get install libx11-dev libxml-dev openjdk-7-jre-headless
-./configure --prefix=/home/dnanexus/R-tmp
-
 }
