fcd2d4cac0d9f165ec4e32db46d7d44b237678b8
Mark Pinese <mark.pinese@gmail.com>
2015-06-29 16:12:31 +1000

    tests: Added printing, to try to identify the cause of the intermittent project not found problem.

--- test/smoke_tests.py
+++ test/smoke_tests.py
@@ -11,16 +11,13 @@ class SmokeTestCase(unittest.TestCase):
         cls.project_id = str(dxpy.PROJECT_CONTEXT_ID)
         cls.project_name = os.environ.get("PROJ_NAME", None)
         cls.project = dxpy.DXProject(dxpy.find_one_project(zero_ok=False, more_ok=False, name=cls.project_name)["id"].encode('ascii', 'ignore'))
-        for output_file in cls.project.list_folder(describe=True)["objects"]:
-            description = output_file["describe"]
-            if description["name"].endswith("pdf"):
-                dxpy.download_dxfile(description["id"], "report.pdf")
 
     # Output pdf must exist and be larger than 0 bytes.
     def test_check_output_files_made(self):
         for output_file in self.project.list_folder(describe=True)["objects"]:
             description = output_file["describe"]
-            if description["name"].endswith("pdf"):
+            if description["name"].endswith(".valrept.pdf"):
+                print description
                 self.assertGreater(description["size"], 0)
                 break
         else:
