0034c1ce045df06e771ac83a69c1716f37568740
Mark Pinese <mark.pinese@gmail.com>
2015-09-03 10:33:05 +1000

    Added quotes to escape a possible * in the command line.

--- src/code.sh
+++ src/code.sh
@@ -81,7 +81,7 @@ main() {
   #
   mkdir -p ~/out/report/ ~/out/rds ~/out/json
   sample_basename=$(basename ${vcfgz_path} .vcf.gz)
-  ./validation_report.sh -o ~/out/report/${sample_basename}.valrept.pdf -d ~/out/rds/${sample_basename}.valrept.rds -j ~/out/json/${sample_basename}.valrept.json -s ${sampleIDs} "${opts[@]}" ${vcfgz_path}
+  ./validation_report.sh -o "~/out/report/${sample_basename}.valrept.pdf" -d "~/out/rds/${sample_basename}.valrept.rds" -j "~/out/json/${sample_basename}.valrept.json" -s "${sampleIDs}" "${opts[@]}" "${vcfgz_path}"
 
   #
   # upload results
