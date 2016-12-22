34e58c860611177f0906b4e09bc5b94feb6a4187
Mark Cowley <m.cowley0@gmail.com>
2016-12-22 21:38:50 +1100

    updated test cases

--- test/create_test_data.sh
+++ test/create_test_data.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -e
 
-dx select kccg
-dx mkdir -p /test/vcfscope-measure/input /test/vcfscope-measure/output
+dx select "VCFscope resources"
 
 dx run vcfscope-measure/9.9.7 \
-    -ivcfgz=/test/kccg-performance-measure/input/GKX_30.realigned.recalibrated.hc.vqsr.chr21.vcf.gz \
-    -ibam=/test/kccg-performance-measure/input/GKX_05.realigned.chr21.aroundvariants.bam \
-    -ibai=/test/kccg-performance-measure/input/GKX_05.realigned.chr21.aroundvariants.bam.bai \
-    -iregion=/test/kccg-performance-measure/input/reportable_range.chr21.bed \
-    --folder /test/vcfscope-measure/output --yes --watch
-# SUCCESS job-Bkkjv3j098GfBQ63GxY099vP
+    -ivcfgz=/test/input/GKX_30.realigned.recalibrated.hc.vqsr.chr21.vcf.gz \
+    -ibam=/test/input/GKX_05.realigned.chr21.aroundvariants.bam \
+    -ibai=/test/input/GKX_05.realigned.chr21.aroundvariants.bam.bai \
+    -iregion=/test/input/reportable_range.chr21.bed \
+    --folder /test/output --yes --watch
+# SUCCESS job-F1JpY9Q0pVj0BgpYBp14f31Q
